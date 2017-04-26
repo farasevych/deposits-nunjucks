52a12c1b9b903808b32b1e37d7ebc057e2d07518
farasevych <farasevych@gmail.com>
2017-04-26 14:22:24 +0300

    footer

--- main.js
+++ main.js
@@ -1,3 +1,2 @@
-console.log('hello world')
 let compiledHtml = nunjucks.render('main.html', { foo: 'bar' });
 document.getElementById('deposits-container').insertAdjacentHTML('afterend', compiledHtml);
