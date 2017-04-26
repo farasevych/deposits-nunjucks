c48e7345787c3ed584cc5b96db50dcb3f39ab3ae
farasevych <farasevych@gmail.com>
2017-04-26 18:14:27 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,2 @@
-let compiledHtml = nunjucks.render('main.html', { foo: 'bar' });
+let compiledHtml = nunjucks.render('../templates/main.html', { foo: 'bar' });
 document.getElementById('deposits-container').insertAdjacentHTML('afterend', compiledHtml);
